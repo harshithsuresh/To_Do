4754a4a2ec25d196f6143f63ab0cff0c1a8a24e9
S <harshiths@hpe.com>
2021-02-05 22:41:15 +0530

    push 5

--- src/app.py
+++ src/app.py
@@ -36,27 +36,49 @@ def delete_task():
     cursor.close()
     return SUCCESS_RESPONSE
 
-@app.route('/update_status', methods=['POST'])
-def update_status():
-    time = datetime.datetime.today()
-    time = time.isoformat()
-    request_data = request.json
-    name = request_data['name']
-    cursor = mysql.connection.cursor()
-    cursor.execute("SELECT STATUS FROM todo where NAME=%s",(name,))
-    row_values = cursor.fetchall()
-    if not len(row_values):
-        return {"status_code": 400,"data": {"message":"To do not found"}}
-    row_value=row_values[0]
-    STATUS=row_value[0]
-    if STATUS in "COMPLETED":
-        cursor.execute("UPDATE todo set STATUS='NOT_COMPLETED',MODIFIED_DATE=%s where NAME=%s",(time,name))
+@app.route('/update/<type>', methods=['POST'])
+def update(type):
+    if "status" in type:
+        time = datetime.datetime.today()
+        time = time.isoformat()
+        request_data = request.json
+        name = request_data['name']
+        cursor = mysql.connection.cursor()
+        cursor.execute("SELECT STATUS FROM todo where NAME=%s",(name,))
+        row_values = cursor.fetchall()
+        if not len(row_values):
+            return {"status_code": 400,"data": {"message":"To do not found"}}
+        row_value=row_values[0]
+        STATUS=row_value[0]
+        if STATUS in "COMPLETED":
+            cursor.execute("UPDATE todo set STATUS='NOT_COMPLETED',MODIFIED_DATE=%s where NAME=%s",(time,name))
+        else:
+            cursor.execute("UPDATE todo set STATUS='COMPLETED',MODIFIED_DATE=%s where NAME=%s",(time,name)) 
+        SUCCESS_RESPONSE['data'] = {"message":"Successful"}  
+        mysql.connection.commit()
+        cursor.close()
+        return SUCCESS_RESPONSE
+    elif "category" in type:
+        request_data = request.json
+        name = request_data['name']
+        ID = request_data['id']
+        cursor = mysql.connection.cursor()
+        cursor.execute('''SELECT ID FROM todo_category''')
+        row_values = cursor.fetchall()
+        for row_value in row_values:
+            if str(ID) in str(row_value[0]):
+                cursor.execute("UPDATE todo SET CATEGORY_ID=%s where NAME=%s",(ID,name))
+                SUCCESS_RESPONSE['data'] = {"message":"Successful"}
+                mysql.connection.commit()
+                cursor.close()
+                return SUCCESS_RESPONSE 
+        mysql.connection.commit()
+        cursor.close()
+        return {"status_code": 409,"data": {"message":"Category does not exists"}}
+
     else:
-        cursor.execute("UPDATE todo set STATUS='COMPLETED',MODIFIED_DATE=%s where NAME=%s",(time,name)) 
-    SUCCESS_RESPONSE['data'] = {"message":"Successful"}  
-    mysql.connection.commit()
-    cursor.close()
-    return SUCCESS_RESPONSE
+        return {"status_code": 400,"data": {"message":"Bad request"}}
+
 
 @app.route('/fetch_category')
 def fetch_category():
